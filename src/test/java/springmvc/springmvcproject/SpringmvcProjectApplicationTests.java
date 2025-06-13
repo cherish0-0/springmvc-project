29283973cac62cd61e822fe0275122fa57d57e2e
cherish0-0 <jiyu01160@naver.com>
2025-06-13 22:15:41 +0900

    Initial commit

---
+++ src/test/java/springmvc/springmvcproject/SpringmvcProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package springmvc.springmvcproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmvcProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
